c62b53dce9d0dc115941bded7f94bc4ff5d5e9ab
Lucas França <djmikejack@hotmail.com>
2020-04-27 08:59:57 -0300

    Projeto iniciado Ola Mundo

---
+++ Curso 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }  
+    }
+}
