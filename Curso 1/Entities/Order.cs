15bbc0d54736e79a315bd79ac2e6042e75650299
Lucas França <lucasgamesdark@gmail.com>
2020-05-01 18:54:29 -0300

    Projeto Fast Food

---
+++ Curso 1/Entities/Order.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Curso_1.Entities.Enums;
+using Curso_1.Entities;
+
+namespace Curso_1.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public Product Product { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client, Product product)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+            Product = product;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            OrderItems.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            OrderItems.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+            foreach (OrderItem item in OrderItems)
+            {
+                total = total + item.SubTotal();
+
+            }
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order moment: ");
+            sb.AppendLine(Moment.ToString());
+            sb.Append("Order status: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.Append(Client.Name);
+            sb.Append(" (");
+            sb.Append(Client.BirthDate.ToString("dd/MM/yyyy"));
+            sb.Append(") - ");
+            sb.AppendLine(Client.Email);
+            sb.AppendLine("Order Items: ");
+            foreach (OrderItem item in OrderItems)
+            {
+                sb.Append(item.Product.Name);
+                sb.Append(", $");
+                sb.Append(item.Price.ToString("F2", CultureInfo.InvariantCulture));
+                sb.Append(", Quantity: ");
+                sb.Append(item.Quantity);
+                sb.Append(", Subtotal: $");
+                sb.AppendLine(item.SubTotal().ToString("F2", CultureInfo.InvariantCulture));
+               
+            }
+            sb.Append("Total price: $");
+            sb.AppendLine(Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
--- Curso 1/Program.cs
+++ Curso 1/Program.cs
@@ -1,8 +1,9 @@
 ﻿
 using System;
-using Curso_1.Entities;
-using Curso_1.Entities.Enums;
 using System.Globalization;
+using Curso_1.Entities.Enums;
+using Curso_1.Entities;
+
 
 
 namespace Curso_1
@@ -11,44 +12,47 @@ namespace Curso_1
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter department´s name: ");
-            string deptName = Console.ReadLine();
-            Console.WriteLine("Enter worker data:");
+            Order order;
+            Console.WriteLine("Enter client data:");
             Console.Write("Name: ");
             string name = Console.ReadLine();
-            Console.Write("Level (Junior/MidLevel/Senior): ");
-            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
-            Console.Write("Base Salary: ");
-            double basesalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MMM/YYYY): ");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            DateTime moment = DateTime.Now;
+            Console.Write("Status: ");
+            Client c = new Client(name, email, birthdate);
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
 
-            Department dept = new Department(deptName);
-            Worker worker = new Worker(name, level, basesalary, dept);
+            Product prod = new Product();
+            order = new Order(moment, status, c, prod);
 
-            Console.Write("How many contracts to this worker? ");
+
+            Console.Write("How many items to this order? ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine("Enter #" + i + " contract data: ");
-                Console.Write("Date: ");
-                DateTime date = DateTime.Parse(Console.ReadLine());
-                Console.Write("Value per hour: ");
-                double valueperhour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.Write("Duration (hours): ");
-                int hours = int.Parse(Console.ReadLine());
-                HourContract contract = new HourContract(date, valueperhour, hours);
-                worker.AddContract(contract);
+                Console.WriteLine("Enter #" + i + " item data:");
+                Console.Write("Product name: ");
+                string nameproduct = Console.ReadLine();
+                Console.Write("Product price: ");
+                double priceproduct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                prod = new Product(nameproduct, priceproduct);
+                OrderItem ord = new OrderItem(quantity, priceproduct, prod);
+                order.AddItem(ord);
             }
+
             Console.WriteLine();
 
-            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
-            string monthandyear = Console.ReadLine();
-            int month = int.Parse(monthandyear.Substring(0, 2));
-            int year = int.Parse(monthandyear.Substring(3));
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
 
-            Console.WriteLine("Name: " + worker.Name);
-            Console.WriteLine("Department: " + worker.Department.Name);
-            Console.WriteLine("Income for " + monthandyear + ": " + worker.Income(year,month).ToString("F2",CultureInfo.InvariantCulture));
+
 
         }
     }
